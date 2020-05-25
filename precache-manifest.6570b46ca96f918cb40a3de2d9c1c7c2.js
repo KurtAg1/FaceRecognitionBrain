f87d1c0b228d2d2f04a8ca8e893f77b998b25186
Kurt <kurtagius613@gmail.com>
2020-05-25 15:45:48 +0200

    Updates

---
+++ precache-manifest.6570b46ca96f918cb40a3de2d9c1c7c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae64416b731f982dc540f8180c71fe84",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "f5337937e16e1376b210",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "357bffaa58f98d772da5",
+    "url": "/facerecognitionbrain/static/css/main.67361177.chunk.css"
+  },
+  {
+    "revision": "f5337937e16e1376b210",
+    "url": "/facerecognitionbrain/static/js/2.49830010.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.49830010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357bffaa58f98d772da5",
+    "url": "/facerecognitionbrain/static/js/main.c7decdcf.chunk.js"
+  },
+  {
+    "revision": "8b0a425d16ee78af64e6",
+    "url": "/facerecognitionbrain/static/js/runtime-main.dbbcf49d.js"
+  }
+]);
